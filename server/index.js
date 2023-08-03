4265bf2666aec919a3ccb145942231dd0e392567
sejal99 <sejal8974@gmail.com>
2023-08-03 14:45:03 +0530

    latest

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors("http://localhost:3000/"));
+app.use(cors("https://sejalsocialmedia.vercel.app/"));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
